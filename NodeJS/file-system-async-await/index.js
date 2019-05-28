5a1210b54a6adf6b2d92934256c76fe18049508e
gabybuchnik <gabsy3@gmail.com>
2019-05-28 13:09:11 +0300

    add nodejs file system operation async await

---
+++ NodeJS/file-system-async-await/index.js
@@ -0,0 +1,51 @@
+const pfs = require('./modules/prom-fs');
+
+const filePath = "./index.html";
+
+const executeReadFileIfExists = async () => {
+    try {
+        await pfs.readFileIfExists(filePath).then(data => console.log(data));
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+const executeWriteFilePromise = async () => {
+    try {
+        await pfs.writeFilePromise(filePath, 'gaby buchnik test 1').then(data => console.log(data))
+    } catch (err) {
+        console.log(err)
+    }
+}
+const executeAppendFilePromise = async () => {
+    try {
+        await pfs.appendFilePromise(filePath, 'change text !!!!').then(data => console.log(data));
+    } catch (err) {
+        console.log(err)
+    }
+}
+const executeReadFilePromise = async () => {
+    try {
+        await pfs.readFilePromise(filePath).then(data => console.log(data));
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+const executeUnlinkPromise = async () => {
+    try {
+        await pfs.unlinkPromise(filePath).then(data => console.log(data));
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+(async () => {
+    await executeReadFileIfExists();
+    await executeWriteFilePromise();
+    await executeReadFileIfExists();
+    await executeAppendFilePromise();
+    await executeReadFileIfExists();
+    await executeUnlinkPromise();
+})();
